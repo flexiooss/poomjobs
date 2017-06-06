43be7acc37b19c382966365608713447d30ea7f3
nelt <nel.taurisson@gmail.com>
2017-06-06 20:36:07 +0000

    [job repository - in memory] move in memory job repo to own class

---
+++ poomjobs-domain/src/main/java/org/codingmatters/poom/poomjobs/domain/repositories/inmemory/InMemoryJobRepository.java
@@ -0,0 +1,46 @@
+package org.codingmatters.poom.poomjobs.domain.repositories.inmemory;
+
+import org.codingmatters.poom.poomjobs.domain.values.JobCriteria;
+import org.codingmatters.poom.poomjobs.domain.values.JobQuery;
+import org.codingmatters.poom.poomjobs.domain.values.JobValue;
+import org.codingmatters.poom.services.domain.exceptions.RepositoryException;
+import org.codingmatters.poom.services.domain.repositories.inmemory.InMemoryRepository;
+import org.codingmatters.poom.servives.domain.entities.Entity;
+import org.codingmatters.poom.servives.domain.entities.PagedEntityList;
+
+import java.util.stream.Stream;
+
+/**
+ * Created by nelt on 6/6/17.
+ */
+public class InMemoryJobRepository extends InMemoryRepository<JobValue, JobQuery> {
+    @Override
+    public PagedEntityList<JobValue> search(JobQuery query, int page, int pageSize) throws RepositoryException {
+        Stream<Entity<JobValue>> filtered = this.stream();
+        for (JobCriteria jobCriteria : query.criteria()) {
+            filtered = this.applyCriterionFilter(filtered, jobCriteria);
+        }
+        return this.paged(filtered, page, pageSize);
+    }
+
+    private Stream<Entity<JobValue>> applyCriterionFilter(Stream<Entity<JobValue>> filtered, JobCriteria jobCriteria) {
+        if (jobCriteria.category() != null) {
+            filtered = filtered.filter(jobValueEntity ->
+                    jobValueEntity.value().category() != null && jobValueEntity.value().category().matches(jobCriteria.category())
+            );
+        }
+        if (jobCriteria.name() != null) {
+            filtered = filtered.filter(jobValueEntity ->
+                    jobValueEntity.value().name() != null && jobValueEntity.value().name().matches(jobCriteria.name())
+            );
+        }
+        if (jobCriteria.runStatus() != null) {
+            filtered = filtered.filter(jobValueEntity ->
+                    jobValueEntity.value().status() != null
+                            && jobValueEntity.value().status().run() != null
+                            && jobValueEntity.value().status().run().matches(jobCriteria.runStatus())
+            );
+        }
+        return filtered;
+    }
+}
