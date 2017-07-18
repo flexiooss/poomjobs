a02ff8649d9186860fcca25686aca9465c67a29e
nelt <nel.taurisson@gmail.com>
2017-07-18 04:12:14 +0000

    [poomjobs] refactored unit tests for JobCollectionDetHandler (only testing specifics as generic collection logic is tested on interface and pager)

--- poomjobs-service/src/test/java/org/codingmatters/poomjobs/service/handlers/JobCollectionGetHandlerTest.java
+++ poomjobs-service/src/test/java/org/codingmatters/poomjobs/service/handlers/JobCollectionGetHandlerTest.java
@@ -1,21 +1,20 @@
 package org.codingmatters.poomjobs.service.handlers;
 
 import org.codingmatters.poom.poomjobs.domain.jobs.repositories.JobRepository;
+import org.codingmatters.poom.poomjobs.domain.values.jobs.JobCriteria;
 import org.codingmatters.poom.poomjobs.domain.values.jobs.JobQuery;
 import org.codingmatters.poom.poomjobs.domain.values.jobs.JobValue;
-import org.codingmatters.poom.poomjobs.domain.values.jobs.jobvalue.Status;
+import org.codingmatters.poom.services.domain.exceptions.RepositoryException;
 import org.codingmatters.poom.services.domain.repositories.Repository;
-import org.codingmatters.poom.servives.domain.entities.Entity;
+import org.codingmatters.poom.services.support.paging.Rfc7233Pager;
 import org.codingmatters.poomjobs.api.JobCollectionGetRequest;
 import org.codingmatters.poomjobs.api.JobCollectionGetResponse;
 import org.codingmatters.poomjobs.api.types.Error;
 import org.codingmatters.poomjobs.service.PoomjobsAPI;
-import org.codingmatters.poomjobs.service.handlers.mocks.MockedJobRepository;
 import org.codingmatters.poomjobs.service.handlers.mocks.MockedRunnerRepository;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -25,247 +24,116 @@ public class JobCollectionGetHandlerTest {
 
     private Repository<JobValue, JobQuery> repository = JobRepository.createInMemory();
     private PoomjobsAPI api = new PoomjobsAPI(this.repository, new MockedRunnerRepository());
+    private JobCollectionGetHandler handler = (JobCollectionGetHandler) this.api.handlers().jobCollectionGetHandler();
+
 
     @Test
-    public void whenNoRangeRequested__ifRepositoryIsEmpty__thenReturnStatus200_andEmptyJobList() throws Exception {
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .build());
-
-        assertThat(response.status200(), is(notNullValue()));
-        assertThat(response.status200().contentRange(), is("Job 0-0/0"));
-        assertThat(response.status200().acceptRange(), is("Job 100"));
-
-        assertThat(response.status200().payload().size(), is(0));
+    public void maxPageSize() throws Exception {
+        assertThat(this.handler.maxPageSize(), is(100));
     }
 
     @Test
-    public void whenNoRangeRequested__ifRepositorySmallerThanDefaultRange__thenReturnStatus200_andCompleteJobList() throws Exception {
-        Entity<JobValue> storedJob = this.repository.create(JobValue.builder()
-                .category("test").name("test").status(Status.builder().run(Status.Run.PENDING).build())
-                .build());
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .build());
-
-        assertThat(response.status200(), is(notNullValue()));
-        assertThat(response.status200().contentRange(), is("Job 0-0/1"));
-        assertThat(response.status200().acceptRange(), is("Job 100"));
-
-        assertThat(response.status200().payload().size(), is(1));
-        assertThat(response.status200().payload().get(0).id(), is(storedJob.id()));
+    public void rfc7233Unit() throws Exception {
+        assertThat(this.handler.rfc7233Unit(), is("Job"));
     }
 
     @Test
-    public void whenNoRangeRequested__ifRepositoryLargerThanDefaultRange__thenReturnStatus206_andPartialJobList() throws Exception {
-        for(int i = 0 ; i < 150 ; i++) {
-            this.repository.create(JobValue.builder()
-                    .category("test").name("test").status(Status.builder().run(Status.Run.PENDING).build())
-                    .build());
+    public void rfc7233Range() throws Exception {
+        assertThat(this.handler.rfc7233Range(JobCollectionGetRequest.builder().range("0-10").build()), is("0-10"));
+    }
+
+    @Test
+    public void whenNameCategoryExitStatusAndRunStatusNonNull__thenJobQuery() throws Exception {
+        assertThat(this.handler.parseQuery(JobCollectionGetRequest.builder()
+                        .name("name")
+                        .category("category")
+                        .exitStatus("FAILED")
+                        .runStatus("DONE")
+                        .build()).criteria(),
+                containsInAnyOrder(
+                        JobCriteria.builder().name("name").build(),
+                        JobCriteria.builder().category("category").build(),
+                        JobCriteria.builder().exitStatus("FAILED").build(),
+                        JobCriteria.builder().runStatus("DONE").build()
+                )
+        );
+    }
+    @Test
+    public void whenAllNameCategoryExitStatusAndRunStatusAreNull__thenJobQueryIsNull() throws Exception {
+        assertThat(this.handler.parseQuery(JobCollectionGetRequest.builder().build()),is(nullValue()));
+    }
+
+    @Test
+    public void partialJobList() throws Exception {
+        for(int i = 0 ; i < 15 ; i++) {
+            this.repository.create(JobValue.builder().name("name-" + i).build());
         }
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .build());
 
+        Rfc7233Pager.Page<JobValue> page = Rfc7233Pager.forRequestedRange("5-9")
+                .unit("String")
+                .maxPageSize(10)
+                .pager(this.repository)
+                .page();
+
+        JobCollectionGetResponse response = this.handler.partialJobList(page);
         assertThat(response.status206(), is(notNullValue()));
-        assertThat(response.status206().contentRange(), is("Job 0-99/150"));
-        assertThat(response.status206().acceptRange(), is("Job 100"));
-
-        assertThat(response.status206().payload().size(), is(100));
+        assertThat(response.status206().payload().size(), is(5));
+        assertThat(response.status206().acceptRange(), is(page.acceptRange()));
+        assertThat(response.status206().contentRange(), is(page.contentRange()));
     }
 
-
     @Test
-    public void whenRangeRequested__ifRepositoryIsEmpty__thenReturnStatus200_andEmptyJobList() throws Exception {
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .build());
+    public void completeList() throws Exception {
+        for(int i = 0 ; i < 10 ; i++) {
+            this.repository.create(JobValue.builder().name("name-" + i).build());
+        }
 
+        Rfc7233Pager.Page<JobValue> page = Rfc7233Pager.forRequestedRange("0-9")
+                .unit("String")
+                .maxPageSize(10)
+                .pager(this.repository)
+                .page();
+
+        JobCollectionGetResponse response = this.handler.completeList(page);
         assertThat(response.status200(), is(notNullValue()));
-        assertThat(response.status200().contentRange(), is("Job 0-0/0"));
-        assertThat(response.status200().acceptRange(), is("Job 100"));
-
-        assertThat(response.status200().payload().size(), is(0));
+        assertThat(response.status200().payload().size(), is(10));
+        assertThat(response.status200().contentRange(), is(page.contentRange()));
+        assertThat(response.status200().acceptRange(), is(page.acceptRange()));
     }
 
     @Test
-    public void whenRangeRequested__ifRangeIsLargerRepository__thenReturnStatus200_andCompleteJobList() throws Exception {
-        Entity<JobValue> storedJob = this.repository.create(JobValue.builder()
-                .category("test").name("test").status(Status.builder().run(Status.Run.PENDING).build())
-                .build());
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .range("0-10")
-                .build());
-
-        assertThat(response.status200(), is(notNullValue()));
-        assertThat(response.status200().contentRange(), is("Job 0-0/1"));
-        assertThat(response.status200().acceptRange(), is("Job 100"));
-
-        assertThat(response.status200().payload().size(), is(1));
-        assertThat(response.status200().payload().get(0).id(), is(storedJob.id()));
-    }
-
-    @Test
-    public void whenRangeRequested__ifRepositoryLargerThanRange__thenReturnStatus206_andPartialJobList() throws Exception {
-        for(int i = 0 ; i < 150 ; i++) {
-            this.repository.create(JobValue.builder()
-                    .category("test").name("test").status(Status.builder().run(Status.Run.PENDING).build())
-                    .build());
+    public void invalidRangeQuery() throws Exception {
+        for(int i = 0 ; i < 10 ; i++) {
+            this.repository.create(JobValue.builder().name("name-" + i).build());
         }
 
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .range("0-9")
-                .build());
+        Rfc7233Pager.Page<JobValue> page = Rfc7233Pager.forRequestedRange("10-9")
+                .unit("String")
+                .maxPageSize(10)
+                .pager(this.repository)
+                .page();
 
-        assertThat(response.status206(), is(notNullValue()));
-        assertThat(response.status206().contentRange(), is("Job 0-9/150"));
-        assertThat(response.status206().acceptRange(), is("Job 100"));
-
-        assertThat(response.status206().payload().size(), is(10));
-    }
-
-    @Test
-    public void whenRangeRequested__ifRangeStartsAfter0__thenReturnStatus206_andOffsettedPartialJobList() throws Exception {
-        Entity<JobValue> startEntity = null;
-        for(int i = 0 ; i < 150 ; i++) {
-            Entity<JobValue> entity = this.repository.create(JobValue.builder()
-                    .category("test").name("test").status(Status.builder().run(Status.Run.PENDING).build())
-                    .build());
-            if(i == 50) {
-                startEntity = entity;
-            }
-        }
-
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .range("50-99")
-                .build());
-
-        assertThat(response.status206(), is(notNullValue()));
-        assertThat(response.status206().contentRange(), is("Job 50-99/150"));
-        assertThat(response.status206().acceptRange(), is("Job 100"));
-
-        assertThat(response.status206().payload().size(), is(50));
-        assertThat(response.status206().payload().get(0).id(), is(startEntity.id()));
-    }
-
-    @Test
-    public void whenRangeRequested__ifRangeIsInvalid__thenResturnStatus406() throws Exception {
-        for(int i = 0 ; i < 150 ; i++) {
-            this.repository.create(JobValue.builder()
-                    .category("test").name("test").status(Status.builder().run(Status.Run.PENDING).build())
-                    .build());
-        }
-
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .range("9-0")
-                .build());
-
+        JobCollectionGetResponse response = this.handler.invalidRangeQuery(page, "error-token");
         assertThat(response.status416(), is(notNullValue()));
-        assertThat(response.status416().acceptRange(), is("Job 100"));
-        assertThat(response.status416().contentRange(), is("Job */150"));
-        assertThat(response.status416().payload().code(), is(Error.Code.ILLEGAL_RANGE_SPEC));
-        assertThat(response.status416().payload().description(), is("start must be before end of range"));
-        assertThat(response.status416().payload().token(), is(notNullValue()));
+        assertThat(
+                response.status416().payload(),
+                is(Error.builder()
+                        .token("error-token")
+                        .description("start must be before end of range")
+                        .code(Error.Code.ILLEGAL_RANGE_SPEC)
+                        .build())
+        );
+        assertThat(response.status416().acceptRange(), is(page.acceptRange()));
+        assertThat(response.status416().contentRange(), is(page.contentRange()));
     }
 
-
-
     @Test
-    public void whenUnexpectedRepositoryException__willReturnAStatus500() throws Exception {
-        JobCollectionGetResponse response = new PoomjobsAPI(new MockedJobRepository(), new MockedRunnerRepository()).handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .build());
+    public void unexpectedError() throws Exception {
+        JobCollectionGetResponse response = this.handler.unexpectedError(new RepositoryException("repo exploded"), "error-token");
 
-        assertThat(response.status500().payload().code(), is(Error.Code.UNEXPECTED_ERROR));
+        assertThat(response.status500(), is(notNullValue()));
+        assertThat(response.status500().payload().token(), is("error-token"));
         assertThat(response.status500().payload().description(), is("unexpected error, see logs"));
-        assertThat(response.status500().payload().token(), is(notNullValue()));
-    }
-
-    @Test
-    public void whenNameParameter__thenOnlyJobWithSuchNameAreReturned() throws Exception {
-        this.repository.create(JobValue.builder()
-                .name("J1").category("C1").status(Status.builder().run(Status.Run.PENDING).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J2").category("C2").status(Status.builder().run(Status.Run.RUNNING).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J3").category("C3").status(Status.builder().run(Status.Run.DONE).exit(Status.Exit.SUCCESS).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J4").category("C4").status(Status.builder().run(Status.Run.DONE).exit(Status.Exit.FAILURE).build())
-                .build());
-
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .name("J2")
-                .build());
-
-        assertThat(response.status200().payload().size(), is(1));
-        assertThat(response.status200().payload().get(0).name(), is("J2"));
-    }
-
-    @Test
-    public void whenCategoryParameter__thenOnlyJobWithSuchCategoryAreReturned() throws Exception {
-        this.repository.create(JobValue.builder()
-                .name("J1").category("C1").status(Status.builder().run(Status.Run.PENDING).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J2").category("C2").status(Status.builder().run(Status.Run.RUNNING).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J3").category("C3").status(Status.builder().run(Status.Run.DONE).exit(Status.Exit.SUCCESS).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J4").category("C4").status(Status.builder().run(Status.Run.DONE).exit(Status.Exit.FAILURE).build())
-                .build());
-
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .category("C3")
-                .build());
-
-        assertThat(response.status200().payload().size(), is(1));
-        assertThat(response.status200().payload().get(0).category(), is("C3"));
-    }
-
-    @Test
-    public void whenRunStatusParameter__thenOnlyJobWithSuchRunStatusAreReturned() throws Exception {
-        this.repository.create(JobValue.builder()
-                .name("J1").category("C1").status(Status.builder().run(Status.Run.PENDING).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J2").category("C2").status(Status.builder().run(Status.Run.RUNNING).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J3").category("C3").status(Status.builder().run(Status.Run.DONE).exit(Status.Exit.SUCCESS).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J4").category("C4").status(Status.builder().run(Status.Run.DONE).exit(Status.Exit.FAILURE).build())
-                .build());
-
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .runStatus("DONE")
-                .build());
-
-        assertThat(response.status200().payload().size(), is(2));
-        assertThat(response.status200().payload().get(0).status().run(), is(org.codingmatters.poomjobs.api.types.job.Status.Run.DONE));
-        assertThat(response.status200().payload().get(1).status().run(), is(org.codingmatters.poomjobs.api.types.job.Status.Run.DONE));
-    }
-
-    @Test
-    public void whenRunExitParameter__thenOnlyJobWithSuchExitStatusAreReturned() throws Exception {
-        this.repository.create(JobValue.builder()
-                .name("J1").category("C1").status(Status.builder().run(Status.Run.PENDING).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J2").category("C2").status(Status.builder().run(Status.Run.RUNNING).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J3").category("C3").status(Status.builder().run(Status.Run.DONE).exit(Status.Exit.SUCCESS).build())
-                .build());
-        this.repository.create(JobValue.builder()
-                .name("J4").category("C4").status(Status.builder().run(Status.Run.DONE).exit(Status.Exit.FAILURE).build())
-                .build());
-
-        JobCollectionGetResponse response = this.api.handlers().jobCollectionGetHandler().apply(JobCollectionGetRequest.builder()
-                .exitStatus("SUCCESS")
-                .build());
-
-        assertThat(response.status200().payload().size(), is(1));
-        assertThat(response.status200().payload().get(0).status().exit(), is(org.codingmatters.poomjobs.api.types.job.Status.Exit.SUCCESS));
+        assertThat(response.status500().payload().code(), is(Error.Code.UNEXPECTED_ERROR));
     }
 }
