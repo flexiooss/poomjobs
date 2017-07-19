0183cbb10a9ef60ccda6da2456f6ceabdfdb5f30
nelt <nel.taurisson@gmail.com>
2017-07-19 20:06:55 +0000

    [poom services] introduced ResourcePutProtocol [poomjobs] refactored JobResourcePutHandler to implement ResourcePutProtocol

--- poomjobs-service/src/main/java/org/codingmatters/poomjobs/service/handlers/JobResourcePutHandler.java
+++ poomjobs-service/src/main/java/org/codingmatters/poomjobs/service/handlers/JobResourcePutHandler.java
@@ -3,9 +3,10 @@ package org.codingmatters.poomjobs.service.handlers;
 import org.codingmatters.poom.poomjobs.domain.jobs.JobValueChange;
 import org.codingmatters.poom.poomjobs.domain.values.jobs.JobQuery;
 import org.codingmatters.poom.poomjobs.domain.values.jobs.JobValue;
+import org.codingmatters.poom.services.domain.change.Change;
 import org.codingmatters.poom.services.domain.exceptions.RepositoryException;
 import org.codingmatters.poom.services.domain.repositories.Repository;
-import org.codingmatters.poom.services.support.logging.LoggingContext;
+import org.codingmatters.poom.services.rest.protocol.ResourcePutProtocol;
 import org.codingmatters.poom.servives.domain.entities.Entity;
 import org.codingmatters.poomjobs.api.JobResourcePutRequest;
 import org.codingmatters.poomjobs.api.JobResourcePutResponse;
@@ -17,17 +18,13 @@ import org.codingmatters.poomjobs.api.types.Error;
 import org.codingmatters.poomjobs.service.JobEntityTransformation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
-
-import java.util.UUID;
-import java.util.function.Function;
 
 import static org.codingmatters.poomjobs.service.JobValueMerger.merge;
 
 /**
  * Created by nelt on 6/15/17.
  */
-public class JobResourcePutHandler implements Function<JobResourcePutRequest, JobResourcePutResponse> {
+public class JobResourcePutHandler implements ResourcePutProtocol<JobValue, JobQuery, JobResourcePutRequest, JobResourcePutResponse> {
     static private final Logger log = LoggerFactory.getLogger(JobResourcePutHandler.class);
 
     private final Repository<JobValue, JobQuery> repository;
@@ -37,60 +34,51 @@ public class JobResourcePutHandler implements Function<JobResourcePutRequest, Jo
     }
 
     @Override
-    public JobResourcePutResponse apply(JobResourcePutRequest request) {
-        try(LoggingContext ctx = LoggingContext.start()) {
-            MDC.put("request-id", UUID.randomUUID().toString());
-            try {
-                Entity<JobValue> entity = this.repository.retrieve(request.jobId());
-                if(entity != null) {
-                    return this.existingJob(request, entity);
-                } else {
-                    return this.unexistingJob(request);
-                }
-            } catch (RepositoryException e) {
-                return this.unexpectedRepositoryError(request, e);
-            }
-        }
+    public String entityId(JobResourcePutRequest request) {
+        return request.jobId();
     }
 
-    private JobResourcePutResponse existingJob(JobResourcePutRequest request, Entity<JobValue> entity) throws RepositoryException {
-        MDC.put("job-id", entity.id());
+    @Override
+    public Logger log() {
+        return log;
+    }
 
+    @Override
+    public Repository<JobValue, JobQuery> repository() {
+        return repository;
+    }
+
+    @Override
+    public Change<JobValue> valueUpdate(JobResourcePutRequest request, Entity<JobValue> entity) {
         JobValue currentValue = entity.value();
         JobValue newValue = merge(currentValue).with(request.payload());
-        JobValueChange change = JobValueChange.from(currentValue).to(newValue);
-
-        if(change.validation().isValid()) {
-            newValue = change.applied();
-
-            entity = this.repository.update(entity, newValue);
-
-            log.info("updated job");
-            return JobResourcePutResponse.builder()
-                    .status200(Status200.builder()
-                            .payload(JobEntityTransformation.transform(entity).asJob())
-                            .build())
-                    .build();
-        } else {
-            String errorToken = UUID.randomUUID().toString();
-            MDC.put("error-token", errorToken);
-            log.info("illegal job change: {}", change.validation().message());
-            return JobResourcePutResponse.builder()
-                    .status400(Status400.builder()
-                            .payload(Error.builder()
-                                    .code(Error.Code.ILLEGAL_JOB_CHANGE)
-                                    .description(change.validation().message())
-                                    .token(errorToken)
-                                    .build())
-                            .build())
-                    .build();
-        }
+        return JobValueChange.from(currentValue).to(newValue);
     }
 
-    private JobResourcePutResponse unexistingJob(JobResourcePutRequest request) {
-        String errorToken = UUID.randomUUID().toString();
-        MDC.put("error-token", errorToken);
-        log.info("no job found with id: {}", request.jobId());
+    @Override
+    public JobResourcePutResponse entityUpdated(Entity<JobValue> entity) {
+        return JobResourcePutResponse.builder()
+                .status200(Status200.builder()
+                        .payload(JobEntityTransformation.transform(entity).asJob())
+                        .build())
+                .build();
+    }
+
+    @Override
+    public JobResourcePutResponse invalidUpdate(Change<JobValue> change, String errorToken) {
+        return JobResourcePutResponse.builder()
+                .status400(Status400.builder()
+                        .payload(Error.builder()
+                                .code(Error.Code.ILLEGAL_JOB_CHANGE)
+                                .description(change.validation().message())
+                                .token(errorToken)
+                                .build())
+                        .build())
+                .build();
+    }
+
+    @Override
+    public JobResourcePutResponse entityNotFound(String errorToken) {
         return JobResourcePutResponse.builder()
                 .status404(Status404.builder()
                         .payload(Error.builder()
@@ -102,13 +90,8 @@ public class JobResourcePutHandler implements Function<JobResourcePutRequest, Jo
                 .build();
     }
 
-    private JobResourcePutResponse unexpectedRepositoryError(JobResourcePutRequest request, RepositoryException e) {
-        String errorToken = UUID.randomUUID().toString();
-        MDC.put("error-token", errorToken);
-
-        log.error("unexpected error while looking up job : {}", request.jobId());
-        log.debug("unexpected exception", e);
-
+    @Override
+    public JobResourcePutResponse unexpectedError(RepositoryException e, String errorToken) {
         return JobResourcePutResponse.builder()
                 .status500(Status500.builder()
                         .payload(Error.builder()
