fab33c57fdade52db98ca60f0c04372483379b35
nelt <nel@flexio.fr>
2019-03-12 17:12:43 +0100

    upgraded to latest snapshots

--- poomjobs-domain/src/main/java/org/codingmatters/poom/poomjobs/domain/jobs/repositories/JobRepository.java
+++ poomjobs-domain/src/main/java/org/codingmatters/poom/poomjobs/domain/jobs/repositories/JobRepository.java
@@ -1,8 +1,12 @@
 package org.codingmatters.poom.poomjobs.domain.jobs.repositories;
 
+import com.mongodb.MongoClient;
+import io.flexio.io.mongo.repository.MongoCollectionRepository;
 import org.codingmatters.poom.poomjobs.domain.jobs.repositories.inmemory.InMemoryJobRepository;
+import org.codingmatters.poom.poomjobs.domain.jobs.repositories.mongo.MongoJobRepositoryFiltersFilters;
 import org.codingmatters.poom.poomjobs.domain.values.jobs.JobQuery;
 import org.codingmatters.poom.poomjobs.domain.values.jobs.JobValue;
+import org.codingmatters.poom.poomjobs.domain.values.jobs.mongo.JobValueMongoMapper;
 import org.codingmatters.poom.services.domain.repositories.Repository;
 
 /**
@@ -11,6 +15,17 @@ import org.codingmatters.poom.services.domain.repositories.Repository;
 public class JobRepository {
     static public Repository<JobValue, JobQuery> createInMemory() {
         return new InMemoryJobRepository();
+    }
+
+    static public Repository<JobValue, JobQuery> createMongo(MongoClient mongoClient, String database) {
+        JobValueMongoMapper jobValueMapper = new JobValueMongoMapper();
+        MongoJobRepositoryFiltersFilters filters = new MongoJobRepositoryFiltersFilters();
+
+        return MongoCollectionRepository.<JobValue, JobQuery>repository(database, "jobs")
+                .withToDocument(jobValueMapper::toDocument)
+                .withToValue(jobValueMapper::toValue)
+                .withFilter(filters::filterJobValues)
+                .build(mongoClient);
     }
 
 }
