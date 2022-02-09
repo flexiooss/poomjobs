05d13407ff19832981de901630e2a51c99261c58
nel <nel@flexio.fr>
2022-02-09 08:09:49 +0100

    [runner service] cleanup

--- poomjobs-runner-service/src/main/java/org/codingmatters/poom/jobs/runner/service/manager/JobRunnerStatusStore.java
+++
@@ -1,9 +0,0 @@
-package org.codingmatters.poom.jobs.runner.service.manager;
-
-import org.codingmatters.poom.jobs.runner.service.exception.UnregisteredTokenException;
-import org.codingmatters.poom.jobs.runner.service.manager.monitor.RunnerStatus;
-import org.codingmatters.poomjobs.runner.domain.RunnerToken;
-
-public interface JobRunnerStatusStore {
-    void statusFor(RunnerToken token, RunnerStatus status) throws UnregisteredTokenException;
-}
