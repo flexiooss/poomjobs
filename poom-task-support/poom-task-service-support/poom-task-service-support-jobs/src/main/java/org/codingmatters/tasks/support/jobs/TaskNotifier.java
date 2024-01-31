0d16e04334e4d283b23a805f1b6a78fa0a1d2fb9
nel <nel@flexio.fr>
2024-01-31 10:25:45 +0100

    [tasks] moved partialResult from ExtendedTaskNotifier to TaskNotifier

--- poom-task-support/poom-task-service-support/poom-task-service-support-jobs/src/main/java/org/codingmatters/tasks/support/jobs/TaskNotifier.java
+++ poom-task-support/poom-task-service-support/poom-task-service-support-jobs/src/main/java/org/codingmatters/tasks/support/jobs/TaskNotifier.java
@@ -2,9 +2,11 @@ package org.codingmatters.tasks.support.jobs;
 
 import org.codingmatters.tasks.api.types.TaskStatusChange;
 import org.codingmatters.tasks.api.types.task.Status;
+import org.codingmatters.value.objects.values.ObjectValue;
 
 public interface TaskNotifier extends TaskLogger {
     void updateRunStatus(TaskStatusChange.Run status);
+    void partialResult(ObjectValue result);
     default TaskLogger withToken(String token) {
         return new TaskLogger() {
             @Override
