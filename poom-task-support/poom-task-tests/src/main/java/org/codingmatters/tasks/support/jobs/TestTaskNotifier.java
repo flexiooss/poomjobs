09f5b3942fb971ce9ae9b60abbe0ca9f6849ec6b
nel <nel@flexio.fr>
2025-07-16 08:45:38 +0200

    log in log

--- poom-task-support/poom-task-tests/src/main/java/org/codingmatters/tasks/support/jobs/TestTaskNotifier.java
+++ poom-task-support/poom-task-tests/src/main/java/org/codingmatters/tasks/support/jobs/TestTaskNotifier.java
@@ -1,5 +1,6 @@
 package org.codingmatters.tasks.support.jobs;
 
+import org.codingmatters.poom.services.logging.CategorizedLogger;
 import org.codingmatters.tasks.api.types.TaskLogCreation;
 import org.codingmatters.tasks.api.types.TaskStatusChange;
 import org.codingmatters.tasks.api.types.task.Status;
@@ -10,6 +11,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 public class TestTaskNotifier implements TaskNotifier {
+    static private final CategorizedLogger log = CategorizedLogger.getLogger(TestTaskNotifier.class);
 
     private final List<TaskLogCreation> logs = Collections.synchronizedList(new LinkedList<>());
     private final List<Status.Run> statusChanges = Collections.synchronizedList(new LinkedList<>());
@@ -17,17 +19,21 @@ public class TestTaskNotifier implements TaskNotifier {
 
     @Override
     public void info(String s, Object ... args) {
-        logs.add(TaskLogCreation.builder().level(TaskLogCreation.Level.INFO).log(s, args).build());
+        TaskLogCreation taskLog = TaskLogCreation.builder().level(TaskLogCreation.Level.INFO).log(s, args).build();
+        logs.add(taskLog);
+        log.info("{}", taskLog);
     }
 
     @Override
     public void error(String s, Object ... args) {
-        logs.add(TaskLogCreation.builder().level(TaskLogCreation.Level.ERROR).log(s, args).build());
+        TaskLogCreation taskLog = TaskLogCreation.builder().level(TaskLogCreation.Level.ERROR).log(s, args).build();
+        logs.add(taskLog);
+        log.error("{}", taskLog);
     }
 
     @Override
     public void updateRunStatus(TaskStatusChange.Run run) {
-        System.out.println("STATUS CHANGED: " + run);
+        log.info("task status changed : {}", run);
         this.statusChanges.add(Status.Run.valueOf(run.name()));
     }
 
